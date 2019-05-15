21b13ffe7bf20106ef70c0af9c1d5cf3959c6e7a
Esteban Segura Guerrero <a01422353@itesm.mx>
2019-05-15 04:32:49 -0500

    Primer enlace con HTML

--- apps/convocatorias/views.py
+++ apps/convocatorias/views.py
@@ -7,3 +7,21 @@ def index_view(request, *args, **kwargs):
     print(request.user)
     #return HttpResponse("<h1>Hola mundo</h1>")
     return render(request, "index.html", {})
+
+def inicio_view(request, *args, **kwargs):
+    return render(request, "inicio.html", {})
+
+def login_view(request, *args, **kwargs):
+    return render(request, "login.html", {})
+
+def registrarse_view(request, *args, **kwargs):
+    return render(request, "newaccount.html", {})
+
+def perfil_view(request, *args, **kwargs):
+    return render(request, "perfil.html", {})
+
+def editarperfil_view(request, *args, **kwargs):
+    return render(request, "editperfil.html", {})
+
+def logaout_view(request, *args, **kwargs):
+    return render(request, "index.html", {})
